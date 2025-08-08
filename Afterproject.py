7bec2669b595c2e2102ded5520f7c813af1df7de
palashwika <paladhiashwika@gmail.com>
2025-08-08 06:12:48 -0700

    Afterproject

---
+++ Afterproject.py
@@ -0,0 +1,10 @@
+age=int(input("Enter your age "))
+
+if age>=10:
+    if age<=20:
+        print("You are between 10 and 20 years old")
+    elif age>20:
+        print("You are above 20 years old")
+elif age<=10:
+    print("You are less than 10 years old")
+
